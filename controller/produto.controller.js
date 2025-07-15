163063d998b62138053aa4cd0d2f49db31fcc261
Eric <eric_scapini@estudante.sesisenai.org.br>
2025-07-14 23:03:32 -0300

    segundo commit

---
+++ controller/produto.controller.js
@@ -0,0 +1,83 @@
+const Produto = require('../model/Produto')
+
+const cadastrar = async (req,res)=>{
+    const dados = req.body
+    try {
+        const valores = await Produto.create(dados)
+        res.status(200).json(valores)
+    } catch (err) {
+        console.error('Erro ao cadastrar o produto!',err)
+        res.status(500).json({message: "Erro ao cadastrar o produto!"})
+    }
+}
+
+const listar = async (req, res) => {
+    try {
+        const valores = await Produto.findAll()
+        res.status(200).json(valores)
+    } catch (err) {
+        console.error('Erro ao listar os produtos!',err)
+        res.status(500).json({message: "Erro ao listar os produtos!"})
+    }
+}
+
+const listarPorId = async (req,res)=>{
+    const id = req.params.id
+    try {
+        const valores = await Produto.findByPk(id)
+        if (valores === null) {
+            console.error('Falha ao tentar encontrar o produto!')
+            res.status(404).json({ message: "Produto não encontrado!"})
+        } else {
+            res.status(200).json(valores)
+            console.log('Produto encontrado com sucesso!')
+        }
+    } catch (err) {
+        console.error('Erro ao buscar produto por ID!', err)
+        res.status(500).json({ message: "Erro ao buscar produto por ID!"})
+    }
+}
+
+const listarPorNome = async (req,res)=>{
+    //Em desenvolvimento, disponível em patches futuros!
+}
+
+const atualizar = async (req,res)=>{
+    const id = req.params.id
+    const dados = req.body
+    try {
+        const valores = await Produto.findByPk(id)
+        if (valores === null) {
+            console.error('Falha ao tentar encontrar o produto!',err)
+            res.status(404).json({message: "Falha ao tentar encontrar produto!"})
+        } else {
+            await Produto.update(dados, {where: {id : id}})
+            const valoresAtual = await Cliente.findByPk(id)
+            res.status(200).json(valoresAtual)
+            console.log('Dados do produto atualizado com sucesso!')
+        }
+    } catch (err) {
+        console.error('Falha ao atualizar dados do produto!',err)
+        res.status(500).json({message: "Falha ao atualizar dados do produto!"})
+    }
+}
+
+const apagar = async (req,res)=>{
+    const id = req.params.id
+    try {
+        const valores = await Produto.findByPk(id)
+        if (valores === null) {
+            console.error('Falha ao tentar encontrar o produto!',err)
+            res.status(404).json({message: "Falha ao tentar encontrar produto!"})
+        } else {
+            await Produto.destroy({where: {id : id}})
+            console.log('Produto apagados com sucesso!')
+            res.status(200).json({message: "Produto apagados com sucesso!"})
+        }
+    } catch (err) {
+        console.error('Falha ao tentar apagar o produto!',err)
+        res.status(500).json({message: "Falha ao tentar apagar o produto!"})
+    }
+}
+
+module.exports = { cadastrar, listar, listarPorId, atualizar, apagar }
---
+++ controller/usuario.controller.js
@@ -0,0 +1,83 @@
+const Usuario = require('../model/Usuario')
+
+const cadastrar = async (req,res)=>{
+    const dados = req.body
+    try {
+        const valores = await Usuario.create(dados)
+        res.status(200).json(valores)
+    } catch (err) {
+        console.error('Erro ao cadastrar o usuário!',err)
+        res.status(500).json({message: "Erro ao cadastrar o usuário!"})
+    }
+}
+
+const listar = async (req, res) => {
+    try {
+        const valores = await Usuario.findAll()
+        res.status(200).json(valores)
+    } catch (err) {
+        console.error('Erro ao listar os usuários!',err)
+        res.status(500).json({message: "Erro ao listar os usuários!"})
+    }
+}
+
+const listarPorId = async (req,res)=>{
+    const id = req.params.id
+    try {
+        const valores = await Usuario.findByPk(id)
+        if (valores === null) {
+            console.error('Falha ao tentar encontrar o usuário!')
+            res.status(404).json({ message: "Usuário não encontrado!" })
+        } else {
+            res.status(200).json(valores)
+            console.log('Usuário encontrado com sucesso!')
+        }
+    } catch (err) {
+        console.error('Erro ao buscar usuário por ID!', err)
+        res.status(500).json({ message: "Erro ao buscar usuário por ID!" })
+    }
+}
+
+const listarPorNome = async (req,res)=>{
+    //Em desenvolvimento, disponível em patches futuros!
+}
+
+const atualizar = async (req,res)=>{
+    const id = req.params.id
+    const dados = req.body
+    try {
+        const valores = await Usuario.findByPk(id)
+        if (valores === null) {
+            console.error('Falha ao tentar encontrar o usuário!',err)
+            res.status(404).json({message: "Falha ao tentar encontrar usuário!"})
+        } else {
+            await Usuario.update(dados, {where: {id : id}})
+            const valoresAtual = await Cliente.findByPk(id)
+            res.status(200).json(valoresAtual)
+            console.log('Dados do usuário atualizado com sucesso!')
+        }
+    } catch (err) {
+        console.error('Falha ao atualizar dados do usuário!',err)
+        res.status(500).json({message: "Falha ao atualizar dados do usuário!"})
+    }
+}
+
+const apagar = async (req,res)=>{
+    const id = req.params.id
+    try {
+        const valores = await Usuario.findByPk(id)
+        if (valores === null) {
+            console.error('Falha ao tentar encontrar o usuário!',err)
+            res.status(404).json({message: "Falha ao tentar encontrar usuário!"})
+        } else {
+            await Usuario.destroy({where: {id : id}})
+            console.log('Dados do usuário apagados com sucesso!')
+            res.status(200).json({message: "Dados do usuário apagados com sucesso!"})
+        }
+    } catch (err) {
+        console.error('Falha ao apagar dados do usuário!',err)
+        res.status(500).json({message: "Falha ao apagar dados do usuário!"})
+    }
+}
+
+module.exports = { cadastrar, listar, listarPorId, atualizar, apagar }
